70c9f3aef6ff63fed62d6d96320cb0c6deb538f7
takwaibrahimmohamed <takwaibrahimmohamed2002@gmail.com>
2025-08-04 22:26:15 +0300

    change

--- src/app/shared/sidebar/sidebar.component.spec.ts
+++ src/app/shared/sidebar/sidebar.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {  ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -9,7 +9,7 @@ describe('SidebarComponent', () => {
   let component: SidebarComponent;
   let fixture: ComponentFixture<SidebarComponent>;
 
-  beforeEach(async(() => {
+  beforeEach((() => {
     TestBed.configureTestingModule({
       declarations: [ SidebarComponent ]
     })
